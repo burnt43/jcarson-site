016446de6651e3b0cd399c6f71ab8357d6341f62
burnt43 <jms.crsn@gmail.com>
2021-12-08 22:58:21 +0000

    removed some reference to some gem I don't care about right now

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# JCARSON: I commented this out to try and run rails with the least amount of
+# extra stuff as possible.
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
